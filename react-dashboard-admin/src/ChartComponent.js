930568d6f9e2153238156d6122096ca5443fc21e
Duvan Mina <duvan.andres312@gmail.com>
2024-07-12 02:15:45 -0500

    Update ChartComponent.js

--- react-dashboard-admin/src/ChartComponent.js
+++ react-dashboard-admin/src/ChartComponent.js
@@ -15,9 +15,9 @@ const ChartComponent = ({ data }) => {
   return (
     <div className="chart-container">
       <div className="browser-filter-container">
-        <label htmlFor="browser-select">Select Browser: </label>
+        <label htmlFor="browser-select">Seleccione el navegador: </label>
         <select id="browser-select" onChange={handleBrowserChange}>
-          <option value="All">All</option>
+          <option value="All">Todos</option>
           {browserNames.map(browser => (
             <option key={browser} value={browser}>{browser}</option>
           ))}
